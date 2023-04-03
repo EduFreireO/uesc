e61d6d25fdea5bb28240310b7ad29c78e28071d7
Eduardo <eduardoestudos12@gmail.com>
2023-04-02 21:09:50 -0300

    Table Tennis

---
+++ estrutura_dados/exercicios/B.Table-Tennis.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int person, streak;
+    cin >> person >> streak;
+
+    int v[person];
+    for(int i = 0; i < person; i++)
+        cin >> v[i];
+
+    int wins = 0, i = 0, j = 1;
+    while(wins != streak)
+    {
+        if(v[j] > v[i])
+        {
+            i = j;
+            wins = 0;
+        }
+        else
+            wins++;
+        j = (j + 1) % person;    
+    }
+    cin >> i;    
+
+}
